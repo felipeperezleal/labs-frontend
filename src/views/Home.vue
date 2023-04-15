f87bf8eed412b20878901878b9f936cf20eb42e8
Felipe Pérez Leal <felipeperezle@gmail.com>
2023-04-14 23:52:48 -0500

    fix home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,12 +24,21 @@
       }
     },
     beforeCreate( ){
-      if( !getAuthenticationToken( ) ){
-        this.$router.push( {name: 'login'} )
-      }
+      if( !getAuthenticationToken( ) ){this.$router.push({name: 'login'})}
     },
     mounted: function(){
-      this.checkRol()
+      axios.get(this.$store.state.backURL+'', {
+          params: { access_token: getAuthenticationToken( )}
+        })
+        .then(response => {
+            this.misroles = response.data;
+            for (const rol of this.misroles) {
+              if(rol.id === 2){
+                this.profesor = true;
+                break;
+              }
+            }
+        });
     },
     methods:{
       addCourse: function(){
@@ -45,27 +54,6 @@
       roles: function(){
         this.$router.push("/principal/roles")
       },
-      checkRol: function(){
-        axios.get(this.$store.state.backURL+'/mis-roles', {
-          params: { access_token: getAuthenticationToken( )}
-        })
-        .then( response => {
-          if( response.status !== 200 ){
-            alert('Error al obtener roles');
-          }else{
-            this.misroles = response.data;
-            for (const rol of this.misroles) {
-              if(rol.id === 2){
-                this.profesor = true;
-                break;
-              }
-            }
-          }
-        }).catch( error => {
-          alert('Error en la petición');
-          console.log(error);
-        } );
-      }
     }
   }
 </script>
